32cbd04c28f791cd5ceb0bd7d746731ee471b15b
Daniel Mass <business.daniel.mass@gmail.com>
2023-06-21 17:50:19 +0200

    Created UserList example for fetching Users from api and deleting them locally

--- src/state/userSlice.ts
+++ src/state/userSlice.ts
@@ -25,10 +25,13 @@ const userSlice = createSlice({
         setUsers: (state, action: PayloadAction<string[]>) => {
             state.users = action.payload;
         },
+        deleteUser(state, action: PayloadAction<string>) {
+            state.users = state.users.filter(x=>x != action.payload);
+        },
     },
 });
 
-export const { addUser,setUsers } = userSlice.actions;
+export const { addUser,setUsers,deleteUser } = userSlice.actions;
 
 export const fetchUsers = (): AppThunk => async (dispatch) => {
     const response = await dispatch(fetchData('http://localhost:3000/clients'));
@@ -37,16 +40,16 @@ export const fetchUsers = (): AppThunk => async (dispatch) => {
 };
 
 
-export const updateUsers = (user: User): AppThunk => async (dispatch) => {
-    dispatch(updateData({
-        url: `https://example.com/users/${user.id}`,
-        payload: user,
-    }));
-};
+// export const updateUsers = (user: User): AppThunk => async (dispatch) => {
+//     dispatch(updateData({
+//         url: `https://example.com/users/${user.id}`,
+//         payload: user,
+//     }));
+// };
 
 
-export const deleteUserApi = (userId: string): AppThunk => async (dispatch) => {
-    dispatch(deleteData(`https://example.com/users/${userId}`));
-};
+// export const deleteUser = (userId: string): AppThunk => async (dispatch) => {
+//     dispatch(deleteData(`https://example.com/users/${userId}`));
+// };
 
 export default userSlice.reducer;
