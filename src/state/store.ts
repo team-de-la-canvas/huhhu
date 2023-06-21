327223121f227f7e18f8689b83db035747593ee3
Daniel Mass <business.daniel.mass@gmail.com>
2023-06-21 08:25:36 +0200

    replaced store with redux toolkit store for simplicity and common practice purpose

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,14 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit'
 
-const initialState = {};
-const middleware = [thunk];
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(...middleware)
-);
+const store = configureStore({
+    reducer: {rootReducer},
+})
 
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
