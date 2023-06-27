97ddc546d06c5949b590e83f295960dd2b6a7ad7
Daniel Mass <business.daniel.mass@gmail.com>
2023-06-27 22:38:50 +0200

    initial interface definition

---
+++ routes.ts
@@ -0,0 +1,72 @@
+import {LocationModel} from "./models";
+
+export interface RegistrationRequest {
+    clientName: string;
+}
+
+export interface RegistrationResponse {
+    clientCode: number;
+}
+
+export interface LoginRequest {
+    clientName: string;
+    clientCode: number;
+}
+
+
+export interface LoginResponse {
+    message: string;
+}
+
+
+export interface MatchRequest {
+    clientCode: number;
+}
+
+
+export interface MatchResponse {
+    message: string;
+    matchName : string|unknown;
+}
+
+export interface ClientsRequest {
+    clientCode: number;
+}
+
+
+export interface ClientsResponse {
+    clients: string[]
+}
+
+
+
+export interface MatchesRequest {
+    clientCode: number;
+}
+
+
+export interface MatchesResponse {
+    matches : string[][];
+}
+
+
+export interface GetLocationOfMatchRequest {
+    clientCode: number;
+}
+
+
+export interface GetLocationOfMatchResponse {
+    location : LocationModel
+    matchName : string;
+}
+
+
+export interface SetLocationRequest {
+    clientCode: number,
+    location : LocationModel
+}
+
+
+export interface SetLocationResponse {
+    location : LocationModel
+}
