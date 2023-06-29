83b3968c74367c970684243ee68537ce92ebfdfc
Daniel Mass <business.daniel.mass@gmail.com>
2023-06-29 09:46:57 +0200

    implemented http status codes & refactored

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import cors from 'cors';
 import {
     ClientsRequest,
@@ -7,7 +7,7 @@ import {
     LoginResponse, MatchesRequest, MatchesResponse,
     MatchRequest, MatchResponse,
     RegistrationRequest,
-    RegistrationResponse
+    RegistrationResponse, SetLocationRequest, SetLocationResponse
 } from "../shared/routes";
 import {Client} from "../shared/models";
 
@@ -15,26 +15,69 @@ import {Client} from "../shared/models";
 const app = express();
 const port = 3000;
 
+
+type ResponseHandlerArgs<ResponseType> = {
+    payload: ResponseType;
+    statusCode: number;
+};
+
+const handleResponseWrapper = <ResponseType>(res: Response, args: ResponseHandlerArgs<ResponseType>) => {
+    const { payload, statusCode } = args;
+    const previousState = JSON.parse(JSON.stringify(clients));
+    res.status(statusCode).send(payload);
+    console.log({
+        action: args,
+        statePreExecution: previousState,
+        statePostExecution: clients
+    });
+};
+
+
+declare global {
+    namespace Express {
+        interface Response {
+            handleResponse: <ResponseType>(args: ResponseHandlerArgs<ResponseType>) => void;
+        }
+    }
+}
+
 app.use(cors());
 app.use(express.json());
 
-let clients: Client[]  = [];
-
-app.get("/", (req:Request, res:Response) => {
-    res.send("Hello World!");
+// Middleware to extend the Response object prototype
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.handleResponse = (args) => handleResponseWrapper(res, args);
+    next();
 });
+
+// Custom middleware to handle exceptions
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // Handle the error and send a response to the client
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+
+
+let clients: Client[]  = [];
 
 // returns on successful login a code for which is used to authenticate clients for future requests
 app.post("/reg", (req: Request<RegistrationRequest>, res: Response<RegistrationResponse | string>) => {
     const clientName = req.body.clientName;
     if (!clientName || clients.find(cl => cl.name === clientName)) {
-        res.send("Error registering under this clientName. Potentially already registered.");
+        res.handleResponse({
+            payload: "Error registering under this clientName. Potentially already registered.",
+            statusCode: 400
+        });
         return;
     }
     const code = Math.floor(Math.random() * 10000);
     clients.push({name: clientName, code: code, location: undefined});
-    res.send({clientCode: code});
-    console.log(clients)
+    
+    res.handleResponse({
+        payload: {clientCode: code},
+        statusCode: 200
+    })
 })
 
 app.post("/visible", (req: Request<LoginRequest>, res: Response<LoginResponse>) => {
@@ -43,20 +86,33 @@ app.post("/visible", (req: Request<LoginRequest>, res: Response<LoginResponse>) 
 
     const clientNames = clients.map(client => client.name);
     const client = clients.find(client => client.name === clientName);
+    
     if (clientNames && client) {
         if (!clientNames.includes(clientName) || client.code !== clientCode) {
-            res.send({
-                message:"error, wrong code"
-            });
+            
+            res.handleResponse({
+                payload: {
+                    message:"error, wrong code"
+                },
+                statusCode: 400
+            })
+            
         } else {
             client.authenticated = true;
-            res.send({
-                message: "successfully logged in!"
+            
+            res.handleResponse({
+                payload: {
+                    message: "successfully logged in!"
+                },
+                statusCode: 200
             });
         }
     } else {
-        res.send({
-            message: "client not registered yet"
+        res.handleResponse({
+            payload: {
+                message: "client not registered yet"
+            },
+            statusCode: 400
         });
     }
 });
@@ -70,27 +126,40 @@ app.post("/match", (req: Request<MatchRequest>, res: Response<MatchResponse>) =>
     if (thisClient && otherClient && thisClient.name !== otherClient.name) {
         thisClient.activeMatchWith = otherClient.name;
         otherClient.activeMatchWith = thisClient.name;
-        res.send({
-            matchName: "otherClient.name", 
-            message: ` successfully matched with other client: ${otherClient.name}`,
+        
+        res.handleResponse({
+            payload: {
+                matchName: otherClient.name,
+                message: `successfully matched with other client: ${otherClient.name}`,
+            },
+            statusCode: 200
         });
-        console.log(clients);
+        
     } else {
-        res.send({message: "client mismatch", matchName: undefined});
+        res.handleResponse({
+            payload: {
+                message: "client mismatch", 
+                matchName: undefined
+            },
+            statusCode: 400
+        });
     }
 })
 
 app.get("/clients", (req: Request<ClientsRequest>, res:Response<ClientsResponse>) => {
     const authenticatedClients = clients.filter(cl => cl.authenticated).map(cl => cl.name);
-    res.json({
-        clients: authenticatedClients
+    res.handleResponse({
+        payload: {
+            clients: authenticatedClients
+        },
+        statusCode: 200
     });
 });
 
 app.get("/matches", (req: Request<MatchesRequest>,res:Response<MatchesResponse>) => {
     const matches = clients.reduce((result:string[][], item) => {
         if (item.activeMatchWith) {
-            // Ueberpruefe, ob das Paar schon im result-Array existiert
+            // Überprüfe, ob das Paar schon im result-Array existiert
             if (!result.some(pair => pair.includes(item.name) && pair.includes(item.activeMatchWith))) {
                 // Wenn nicht, fuege das Paar hinzu
                 result.push([item.name, item.activeMatchWith].sort());
@@ -98,17 +167,25 @@ app.get("/matches", (req: Request<MatchesRequest>,res:Response<MatchesResponse>)
         }
         return result;
     }, [])
-    res.send({
-        matches: matches
+    res.handleResponse({
+        payload: {
+            matches: matches
+        },
+        statusCode: 200
     });
 })
 
 
-app.post("/setLocation", (req, res) => { 
+app.post("/setLocation", (req: Request<SetLocationRequest>, res: Response<SetLocationResponse>) => { 
     const clientCode = req.body.clientCode;
     const thisClient = clients.find(cl => cl.authenticated && cl.code === clientCode);
     thisClient.location = req.body.clientLocation;
-    res.send(req.body.clientLocation)
+    res.handleResponse({
+        payload: {
+            location: req.body.clientLocation
+        },
+        statusCode: 200
+    });
 })
 
 
@@ -116,14 +193,20 @@ app.post("/getLocationOfMatch", (req, res) => {
     const clientCode = req.body.clientCode;
     const thisClient = clients.find(cl => cl.authenticated && cl.code === clientCode);
     const otherClient = clients.find(cl => cl.authenticated && cl.activeMatchWith === thisClient.name);
-    res.send({
-        clientLocation:otherClient.location 
+    res.handleResponse({
+        payload: {
+            clientLocation:otherClient.location
+        },
+        statusCode: 200
     })
 })
 
 app.post("/debugSetState", (req : Request<Client[]>, res : Response<Client[]>) => {
     clients = req.body;
-    res.send(req.body)
+    res.handleResponse({
+        payload:req.body,
+        statusCode: 200
+    })
 });
 
 app.listen(port, () => {
