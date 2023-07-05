9d334939af8a46a4b49628f99ca5b8fdba4c940e
Daniel Mass <business.daniel.mass@gmail.com>
2023-07-05 12:28:42 +0200

    move configuration to app.config.js, so its dynamic

--- app.config.js
+++ app.config.js
@@ -2,5 +2,22 @@ module.exports = {
     version: process.env.MY_CUSTOM_PROJECT_VERSION || '1.0.0',
     extra: {
         api_url: process.env.API_URL ||"https://huhhu.app",
+        eas: {
+            projectId: "d506ab00-51ff-46e7-94ca-e2475d1f388e"
+        }
     },
+    android: {
+        package: "app.huhhu"
+    },
+    ios: {
+        bundleIdentifier: "app.huhhu"
+    },
+    plugins: [
+        [
+            "expo-location",
+            {
+                locationAlwaysAndWhenInUsePermission: "Allow app.huhhu to use your location."
+            }
+        ]
+    ]
 };
