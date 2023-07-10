494b45fb5cbe46684e9616811c96de15e118a27a
Daniel Mass <business.daniel.mass@gmail.com>
2023-07-10 23:43:44 +0200

    arrow is moving

--- src/components/Pointer.tsx
+++ src/components/Pointer.tsx
@@ -1,20 +1,18 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {Animated, Easing, StyleSheet, Text,Image} from 'react-native';
-import {useHuntingSelector} from "../state/huntingSlice";
-
-
+import React, { useRef, useEffect, useState } from 'react';
+import { Animated, Easing, StyleSheet, Text, Image } from 'react-native';
+import { useHuntingSelector } from "../state/huntingSlice";
 
 const Pointer = () => {
     const rotation = useRef(new Animated.Value(0)).current;
-    const [valid,setValid] = useState(false);
+    const [valid, setValid] = useState(false);
 
-    const myLocation = useHuntingSelector(state =>state.myLocation);
-    const otherLocation = useHuntingSelector(state =>state.otherLocation);
+    const myLocation = useHuntingSelector(state => state.myLocation);
+    const otherLocation = useHuntingSelector(state => state.otherLocation);
+
     useEffect(() => {
-        if (myLocation&&otherLocation) {
+        if (myLocation && otherLocation) {
             setValid(true);
             const angleDeg = calculateAngle();
-            console.log(rotation)
             Animated.timing(rotation, {
                 toValue: angleDeg,
                 duration: 2000,
@@ -34,32 +32,30 @@ const Pointer = () => {
         brng = brng * (180 / Math.PI);  // convert from radians to degrees
         brng = (brng + 360) % 360;
         brng = 360 - brng; // count degrees counter-clockwise - remove to make clockwise
+        console.log("calculated angle: ", brng);
         return brng;
     }
 
+    const Animation = () => (
+        <Animated.View style={[styles.animationContainer, { transform: [{ rotate: rotation.interpolate({
+                    inputRange: [0, 360],
+                    outputRange: ['0deg', '360deg']
+                }) }] }]}>
+            <Image source={require('../../assets/arrow-up.png')} style={{ width: 500, height: 500 }} />
+        </Animated.View>
+    );
 
-    const transformStyle = [{
-        transform: [{ rotate: `${rotation}deg` }]
-    }];
-
-    const Animation = () => 
-        <Animated.Text style={[styles.textStyle, transformStyle]}>
-            <Image source={require('../../assets/arrow-up.png')} style={{ width: 500, height: 500 }}/>
-        </Animated.Text>
-    
     return (
         <>
-            {valid?<Animation/>:<Text>No navigation possible</Text>}
+            {valid ? <Animation /> : <Text>No navigation possible</Text>}
         </>
     );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: 'blue',
-        // Add any other styles you want to apply
+    animationContainer: {
+        width: 500,
+        height: 500,
     },
 });
 
